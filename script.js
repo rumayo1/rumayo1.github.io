3953be3d243f68dad529d384d8247c6eba170a71
rumayo1 <rzha@risd.edu>
2025-02-03 13:41:25 -0500

    page
    js ai

---
+++ script.js
@@ -0,0 +1,49 @@
+let faceMesh;
+let video;
+let faces = [];
+let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
+let faceDetails;
+const faceDetailsDiv = document.querySelector('.faceDetails');
+
+
+function preload() {
+  // Load the faceMesh model
+  faceMesh = ml5.faceMesh(options);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  // Start detecting faces from the webcam video
+  faceMesh.detectStart(video, gotFaces);
+}
+
+function draw() {
+  // Draw the webcam video
+  //image(video, 0, 0, width, height);
+  background('black');
+
+  // Draw all the tracked face points
+  for (let i = 0; i < faces.length; i++) {
+    let face = faces[i];
+    for (let j = 0; j < face.keypoints.length; j++) {
+      let keypoint = face.keypoints[j];
+      fill(0, 255, 0);
+      noStroke();
+      circle(keypoint.x, keypoint.y, 5);
+    }
+  }
+}
+
+// Callback function for when faceMesh outputs data
+function gotFaces(results) {
+  // Save the output to the faces variable
+  faces = results;
+  faceDetails = results[0].faceOval;
+  console.log(faceDetails);
+  faceDetailsDiv.innerText = "X: "+faceDetails.x+" Y: "+faceDetails.y;
+}
+
